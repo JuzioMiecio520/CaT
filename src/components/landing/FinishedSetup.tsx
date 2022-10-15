c9efd64739f31a519279ea909610f6b675196921
JuzioMieio520 <mikolaj.olszowy@gmail.com>
2022-10-15 11:55:30 +0200

    chore: add patches for pretty-quick

--- src/components/landing/FinishedSetup.tsx
+++ src/components/landing/FinishedSetup.tsx
@@ -4,105 +4,106 @@ import { IconBrandGithub } from '@tabler/icons';
 const BREAKPOINT = '@media (max-width: 755px)';
 
 const useStyles = createStyles((theme) => ({
-    wrapper: {
-        position: 'relative',
-        boxSizing: 'border-box',
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  wrapper: {
+    position: 'relative',
+    boxSizing: 'border-box',
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+
+  inner: {
+    position: 'relative',
+    paddingTop: 200,
+    paddingBottom: 120,
+
+    [BREAKPOINT]: {
+      paddingBottom: 80,
+      paddingTop: 80,
     },
+  },
 
-    inner: {
-        position: 'relative',
-        paddingTop: 200,
-        paddingBottom: 120,
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 62,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    margin: 0,
+    padding: 0,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
 
-        [BREAKPOINT]: {
-            paddingBottom: 80,
-            paddingTop: 80,
-        },
+    [BREAKPOINT]: {
+      fontSize: 42,
+      lineHeight: 1.2,
     },
+  },
 
-    title: {
-        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-        fontSize: 62,
-        fontWeight: 900,
-        lineHeight: 1.1,
-        margin: 0,
-        padding: 0,
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  description: {
+    marginTop: theme.spacing.xl,
+    fontSize: 24,
 
-        [BREAKPOINT]: {
-            fontSize: 42,
-            lineHeight: 1.2,
-        },
+    [BREAKPOINT]: {
+      fontSize: 18,
     },
+  },
 
-    description: {
-        marginTop: theme.spacing.xl,
-        fontSize: 24,
+  controls: {
+    marginTop: theme.spacing.xl * 2,
 
-        [BREAKPOINT]: {
-            fontSize: 18,
-        },
+    [BREAKPOINT]: {
+      marginTop: theme.spacing.xl,
     },
+  },
 
-    controls: {
-        marginTop: theme.spacing.xl * 2,
+  control: {
+    height: 54,
+    paddingLeft: 38,
+    paddingRight: 38,
 
-        [BREAKPOINT]: {
-            marginTop: theme.spacing.xl,
-        },
+    [BREAKPOINT]: {
+      height: 54,
+      paddingLeft: 18,
+      paddingRight: 18,
+      flex: 1,
     },
-
-    control: {
-        height: 54,
-        paddingLeft: 38,
-        paddingRight: 38,
-
-        [BREAKPOINT]: {
-            height: 54,
-            paddingLeft: 18,
-            paddingRight: 18,
-            flex: 1,
-        },
-    },
+  },
 }));
 
 export default function FinishedSetup() {
-    const { classes } = useStyles();
+  const { classes } = useStyles();
 
-    return (
-        <div className={classes.wrapper}>
-            <Container size={700} className={classes.inner}>
-                <h1 className={classes.title}>
-                    CAT cloud service
-                </h1>
+  return (
+    <div className={classes.wrapper}>
+      <Container size={700} className={classes.inner}>
+        <h1 className={classes.title}>CAT cloud service</h1>
 
-                <Text className={classes.description} color="dimmed">
-                    CAT is a self-hosted home cloud service. To view your dashboard or manage this installation please log in. If you don't have an account, ask the CAT instance owner to create on for you or host it yourself!
-                </Text>
+        <Text className={classes.description} color="dimmed">
+          CAT is a self-hosted home cloud service. To view your dashboard or
+          manage this installation please log in. If you don't have an account,
+          ask the CAT instance owner to create on for you or host it yourself!
+        </Text>
 
-                <Group className={classes.controls}>
-                    <Button
-                        size="xl"
-                        className={classes.control}
-                        variant="gradient"
-                        gradient={{ from: 'blue', to: 'cyan' }}
-                    >
-                        Login
-                    </Button>
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+          >
+            Login
+          </Button>
 
-                    <Button
-                        component="a"
-                        href="https://github.com/mantinedev/mantine"
-                        size="xl"
-                        variant="default"
-                        className={classes.control}
-                        leftIcon={<IconBrandGithub size={20} />}
-                    >
-                        View source on GitHub
-                    </Button>
-                </Group>
-            </Container>
-        </div>
-    );
+          <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftIcon={<IconBrandGithub size={20} />}
+          >
+            View source on GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
 }
